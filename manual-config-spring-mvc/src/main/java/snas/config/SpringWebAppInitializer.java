a88080ba48f90989a8aa3befa788dddb954b379a
Vadym Perepeliak <vapsel@gmail.com>
2017-03-26 14:22:33 +0200

    - Manual config of spring mvc implemented - Tested with tomcat 8 - Refactor changes in gradle scripts

---
+++ manual-config-spring-mvc/src/main/java/snas/config/SpringWebAppInitializer.java
@@ -0,0 +1,39 @@
+package snas.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * Configuration of {@link org.springframework.web.servlet.DispatcherServlet}
+ * This class replace web.xml
+ */
+public class SpringWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    /**
+     * Maps DispatcherServlet to root i.e. it will handle all requests
+     * @return Root path
+     */
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    /**
+     * Configures the application context created by {@link org.springframework.web.context.ContextLoaderListener}
+     * @return Config class
+     */
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{RootConfig.class};
+    }
+
+    /**
+     * Configures DispatcherServlet i.e. beans
+     * @return Config class
+     */
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[]{WebConfig.class};
+    }
+
+
+}
