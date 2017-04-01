75d174e6c6a3df3c02ab7501a8993112089f21d9
plague <Ashaxpress295>
2017-04-01 02:00:38 +0100

    Changed AjaxController RequestMapping url and two tests for it.

--- spring-boot-sample/src/main/java/smas/spring/boot/sample/AjaxController.java
+++ spring-boot-sample/src/main/java/smas/spring/boot/sample/AjaxController.java
@@ -1,41 +1,50 @@
 package smas.spring.boot.sample;
 
 import com.restfb.DefaultFacebookClient;
-import com.restfb.FacebookClient;
-import com.restfb.JsonMapper;
 import com.restfb.types.User;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
-@Controller
+@RestController
 public class AjaxController {
-    @RequestMapping(value="/login")
-    public void receiveResponse(@RequestBody String json, HttpServletRequest request){
+
+    @RequestMapping(value = "/userJson", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> receiveResponse(@RequestBody String json, HttpServletRequest request) {
         User user = parseUserData(json);
 
         printUserInfo(user);
+
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
 
     private User parseUserData(String jsonString) {
-        FacebookClient fbclient = new DefaultFacebookClient();
+        /*FacebookClient fbclient = new DefaultFacebookClient();
         JsonMapper mapper = fbclient.getJsonMapper();
-        User user = mapper.toJavaObject(jsonString, User.class);
+        User user = mapper.toJavaObject(jsonString, User.class);*/
 
-        return user;
+        return new DefaultFacebookClient().getJsonMapper().toJavaObject(jsonString, User.class);
     }
 
     private void printUserInfo(User user){
-        String info =
-                "Name: " + user.getName() + "\n" +
-                "Hometown: " + user.getHometown().getName() + "\n" +
-                "Location: " + user.getLocation().getName() + "\n" +
-                "Work: " + user.getWork() + "\n" +
-                "Likes: " + user.getLikes() + "\n";
+        try {
+            String info =
+                    "Name: " + user.getName() + "\n" +
+                            "Hometown: " + user.getHometown().getName() + "\n" +
+                            "Location: " + user.getLocation().getName() + "\n" +
+                            "Work: " + user.getWork() + "\n" +
+                            "Likes: " + user.getLikes() + "\n";
 
-        System.out.print(info);
+            System.out.print(info);
+        }catch (NullPointerException e){
+            //of course, it is a very bad way, but user produces too much null's
+        }
     }
 
 }
--- spring-boot-sample/src/test/java/smas/spring/boot/sample/ControllersTest.java
+++ spring-boot-sample/src/test/java/smas/spring/boot/sample/ControllersTest.java
@@ -1,13 +1,15 @@
 package smas.spring.boot.sample;
 
-
 import org.junit.Test;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
-
 
 public class ControllersTest {
     @Test
@@ -26,4 +28,28 @@ public class ControllersTest {
         mockMvc.perform(get("/offers")).andExpect(view().name("html/offers"));
     }
 
+    @Test
+    public void testLoginModels() throws Exception {
+        LoginController controller = new LoginController();
+        MockMvc mockMvc = standaloneSetup(controller).build();
+
+        mockMvc.perform(get("/login"))
+                .andExpect(model().attributeExists("scope"))
+                .andExpect(model().attribute("scope", "user_hometown, user_location, user_about_me, email, public_profile"));
+
+
+    }
+
+    @Test
+    public void testAjaxController() throws Exception {
+        AjaxController controller = new AjaxController();
+        MockMvc mockMvc = standaloneSetup(controller).build();
+
+        String testJson = "{\"name\":\"First Last\",\"id\":\"7777777777777777\"}";
+
+        mockMvc.perform(post("/userJson")
+                .accept(MediaType.APPLICATION_JSON)
+                .content(testJson))
+               .andExpect(status().isAccepted());
+    }
 }
