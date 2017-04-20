5245f159bc4c2d8118559a4bdf9e870d0f04ff5c
Vadym Perepeliak <vapsel@gmail.com>
2017-04-20 22:33:15 +0200

    Categories search implementation

--- smas-core/src/main/java/smas/core/database/repository/GraphRepository.java
+++ smas-core/src/main/java/smas/core/database/repository/GraphRepository.java
@@ -1,10 +1,17 @@
 package smas.core.database.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import smas.core.database.domain.CategoryData;
 import smas.core.database.domain.IntelligentNodeData;
+
+import java.util.List;
 
 @Repository
 public interface GraphRepository extends JpaRepository<IntelligentNodeData, Long>{
+
+    @Query("Select c FROM CategoryData c")
+    List<CategoryData> findAllCategories();
 
 }
--- smas-core/src/main/java/smas/core/database/service/interfaces/GraphService.java
+++ smas-core/src/main/java/smas/core/database/service/interfaces/GraphService.java
@@ -1,8 +1,25 @@
 package smas.core.database.service.interfaces;
 
+import smas.core.database.domain.CategoryData;
 import smas.core.database.domain.IntelligentNodeData;
+
+import java.util.List;
 
 public interface GraphService {
 
-    public void save(IntelligentNodeData node);
+    /**
+     * Persists node in database.
+     * <p>
+     *     If node already has id, it will be updated.
+     *     If node doesn't have id, new entity will be created.
+     * </p>
+     * @param node Node to persists
+     */
+    void save(IntelligentNodeData node);
+
+    /**
+     * Find all available categories (used and unused).
+     * @return List of categories
+     */
+    List<CategoryData> findAllCategories();
 }
